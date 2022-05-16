e2156729d424b4309f4152318ee1bf277f833536
kartikey <kartikeykumar150>
2022-05-16 22:38:46 +0530

    Removed the loading of the food items

--- src/components/favorites/favorites.js
+++ src/components/favorites/favorites.js
@@ -3,7 +3,6 @@ import getPhoto from "../../apiRequest/getPhoto";
 import { Link } from "react-router-dom";
 
 const Favorites = ({ items }) => {
-
   const [images, setImages] = useState([]);
   const [imagesLoaded, setImagesLoaded] = useState(false);
   const recipeLoaded = true;
@@ -33,32 +32,28 @@ const Favorites = ({ items }) => {
   return (
     <div>
       <div className="resultDiv">
-        {recipeLoaded ? (
-          items.map((item, index) => {
-            return (
-              <div key={index}>
-                {imagesLoaded && (
-                  <img
-                    src={
-                      images[index].photos[0]
-                        ? images[index].photos[0].src.large
-                        : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F07%2F13%2FUltimate-Veggie-Burgers-FT-Recipe-0821.jpg"
-                    }
-                    alt={images[index].photos.alt}
-                  />
-                )}
-                <h5 id="itemHeading">{item.title}</h5>
-                <p id="ingredientsOverview">{`${item.ingredients.substring(
-                  0,
-                  100
-                )}...`}</p>
-                <div className="resultItemNav"></div>
-              </div>
-            );
-          })
-        ) : (
-          <p>Loading...</p>
-        )}
+        {items.map((item, index) => {
+          return (
+            <div key={index}>
+              {imagesLoaded && (
+                <img
+                  src={
+                    images[index].photos[0]
+                      ? images[index].photos[0].src.large
+                      : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F07%2F13%2FUltimate-Veggie-Burgers-FT-Recipe-0821.jpg"
+                  }
+                  alt={images[index].photos.alt}
+                />
+              )}
+              <h5 id="itemHeading">{item.title}</h5>
+              <p id="ingredientsOverview">{`${item.ingredients.substring(
+                0,
+                100
+              )}...`}</p>
+              <div className="resultItemNav"></div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
