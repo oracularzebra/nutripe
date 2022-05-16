7ae547f12183ef0d56ec4b9d506cee9b2b277993
kartikey <kartikeykumar150>
2022-05-16 23:07:16 +0530

    Food page appears when a recipe is selected

--- src/components/favorites/favorites.js
+++ src/components/favorites/favorites.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import getPhoto from "../../apiRequest/getPhoto";
+import "./favorite.css";
 import { Link } from "react-router-dom";
 
-const Favorites = ({ items }) => {
+const Favorites = ({ items, setItem }) => {
   const [images, setImages] = useState([]);
   const [imagesLoaded, setImagesLoaded] = useState(false);
 
@@ -30,26 +31,41 @@ const Favorites = ({ items }) => {
 
   return (
     <div>
-      <div className="resultDiv">
+      <div className="favoriteDiv">
         {items.map((item, index) => {
           return (
             <div key={index}>
-              {imagesLoaded && (
-                <img
-                  src={
-                    images[index].photos[0]
-                      ? images[index].photos[0].src.large
-                      : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F07%2F13%2FUltimate-Veggie-Burgers-FT-Recipe-0821.jpg"
+              <Link
+                to={`${item.title}`}
+                onClick={() => {
+                  if (imagesLoaded) {
+                    const newItem = {
+                      ...item,
+                      image: images[index].photos[0].src.large,
+                    };
+                    setItem(newItem);
+                  } else {
+                    setItem(item);
                   }
-                  alt={images[index].photos.alt}
-                />
-              )}
-              <h5 id="itemHeading">{item.title}</h5>
-              <p id="ingredientsOverview">{`${item.ingredients.substring(
-                0,
-                100
-              )}...`}</p>
-              <div className="resultItemNav"></div>
+                }}
+              >
+                {imagesLoaded && (
+                  <img
+                    src={
+                      images[index].photos[0]
+                        ? images[index].photos[0].src.large
+                        : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F07%2F13%2FUltimate-Veggie-Burgers-FT-Recipe-0821.jpg"
+                    }
+                    alt={images[index].photos.alt}
+                  />
+                )}
+                <h5 id="itemHeading">{item.title}</h5>
+                <p id="ingredientsOverview">{`${item.ingredients.substring(
+                  0,
+                  100
+                )}...`}</p>
+                <div className="resultItemNav"></div>
+              </Link>
             </div>
           );
         })}
