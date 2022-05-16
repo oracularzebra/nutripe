c20efdb732846141da18ada273ed93a53dd6e4a6
kartikey <kartikeykumar150>
2022-05-16 10:50:38 +0530

    Added the favorites page to the route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
-import React from "react";
+import React, { useReducer } from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import { Routes, Route } from "react-router";
 import Result from "./components/searchResult/Result";
 import Item from "./components/food/food";
 import Main from "./components/homePage/home";
+import Favorites from "./components/favorites/Favorites";
 
 function App() {
   const navigate = useNavigate();
   const [value, setValue] = useState("");
   const [item, setItem] = useState({});
+
+  //favorites is a stateful value, and dispatch is a function for updating the value.
+  //favoriteReducer performs all the crud operations.
+  function favoriteReducer(favorites, action) {
+    switch (action.type) {
+      case "add": {
+        console.log("added");
+        break;
+      }
+      case "remove": {
+        console.log("removed");
+        break;
+      }
+    }
+  }
+
+  const [favorites, dispach] = useReducer(
+    favoriteReducer,
+    JSON.parse(localStorage.getItem("favorites") || 
+    []
+    )
+  );
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -27,15 +50,19 @@ function App() {
         setValue={setValue}
       ></Header>
       <Routes>
-        <Route
-          path="/"
-          element={<Main/>}
-        ></Route>
+        <Route path="/" element={<Main />}></Route>
         <Route
           path="/search/:name"
           element={<Result searchQuery={value} setItem={setItem} />}
         ></Route>
-        <Route path="/:name/:id" element={<Item item={item} setItem={setItem}/>} />
+        <Route
+          path="/:name/:id"
+          element={<Item item={item} setItem={setItem} />}
+        />
+        <Route
+          path="/favorites"
+          element={<Favorites items={favorites} />}
+        ></Route>
         <Route path="*"></Route>
       </Routes>
       <Footer></Footer>
