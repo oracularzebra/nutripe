bbb52c7dddbfb5de0a69e63df0bdde6ae45c8d08
kartikey <kartikeykumar150>
2022-05-16 23:10:21 +0530

    Added the trailling * after the /

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ root.render(
   <React.StrictMode>
     <Router> 
       <Routes>
-        <Route path='*' element={<App/>}>
+        <Route path='/*' element={<App/>}>
         </Route>
       </Routes>
     </Router>
