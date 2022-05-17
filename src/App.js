990654a6f578bf70a23ae493778a3201014a6948
kartikey <kartikeykumar150>
2022-05-17 10:16:47 +0530

    Removed unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
-import React, { useReducer } from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router";
+import React from "react";
 import { Routes, Route } from "react-router";
 import Result from "./components/searchResult/Result";
 import Item from "./components/food/food";
