e591dd4cc74603430b03a591e6618d1cfcf1598d
kartikey <kartikeykumar150>
2022-05-22 09:25:42 +0530

    removed the second recipe api and exported the recipe-by-api-ninja

--- src/apiRequest/getRecipes.js
+++ src/apiRequest/getRecipes.js
@@ -18,24 +18,5 @@ const getRecipesFromApiNinjas = async (food) => {
     return console.log(reject);
   }
 };
-const getRecipesFromRecipesApi = async (food) => {
-  const options = { 
-    method: "GET",
-    url: "https://recipesapi2.p.rapidapi.com/recipes/tomato%20soup",
-    params: { maxRecipes: "10" },
-    headers: {
-      "X-RapidAPI-Host": "recipesapi2.p.rapidapi.com",
-      "X-RapidAPI-Key": "f6d53b64f0msh477f1931b2416ebp1c34e9jsnde49544a7ea4",
-    },
-  };
 
-  axios
-    .request(options)
-    .then(function (response) {
-      return response.data;
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-};
-export default getRecipesFromRecipesApi || getRecipesFromApiNinjas;
+export default getRecipesFromApiNinjas;
