e633df111cc8d4c8898e8e0cb656f2293a91aabc
kartikey <kartikeykumar150>
2022-05-22 19:44:04 +0530

    Implemented the cross in food ingredient list and added circular progress bar in favorites page

--- src/components/favorites/favorites.js
+++ src/components/favorites/favorites.js
@@ -3,9 +3,10 @@ import getPhoto from "../../apiRequest/getPhoto";
 import { Link } from "react-router-dom";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { FoodContext } from "../context/foodContext";
+import { Box } from "@material-ui/core";
+import { CircularProgress } from "@mui/material";
 
 const Favorites = () => {
-
   const { favorites, setItem, handleFavoriteButton } = useContext(FoodContext);
   const [images, setImages] = useState([]);
   const [imagesLoaded, setImagesLoaded] = useState(false);
@@ -34,55 +35,79 @@ const Favorites = () => {
   return (
     <div>
       <div className="grid grid-cols-1 gap-4 m-2 md:grid-cols-5 md:m-1 md:mt-0 justify-center bg-slate-200">
-        {favorites.map((item, index) => {
-          return (
-            <div className="grid content-between justify-center items-center rounded-md border-2 border-blue-200" key={index}>
-              <Link
-                to={`${item.title}`}
-                onClick={() => {
-                  if (imagesLoaded) {
-                    const newItem = {
-                      ...item,
-                      image: images[index].photos[0].src.large,
-                    };
-                    setItem(newItem);
-                  } else {
-                    setItem(item);
-                  }
-                }}
+        {imagesLoaded ? (
+          favorites.map((item, index) => {
+            return (
+              <div
+                className="grid content-between justify-center items-center rounded-md border-2 border-blue-200"
+                key={index}
               >
-                {imagesLoaded && (
-                  <img
-                    className="h-96 object-cover w-screen md:h-40"
-                    src={
-                      images[index].photos[0]
-                        ? images[index].photos[0].src.large
-                        : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F07%2F13%2FUltimate-Veggie-Burgers-FT-Recipe-0821.jpg"
+                <Link
+                  to={`${item.title}`}
+                  onClick={() => {
+                    if (imagesLoaded) {
+                      const newItem = {
+                        ...item,
+                        image: images[index].photos[0].src.large,
+                      };
+                      setItem(newItem);
+                    } else {
+                      setItem(item);
                     }
-                    alt={images[index].photos.alt}
-                  />
-                )}
-                <h5 className="text-center font-serif font-bold" id="itemHeading">{item.title}</h5>
-                <p className="text-xs text-center font-light">{`${item.ingredients.substring(
-                  0,
-                  100
-                )}...`}</p>
-                <div className="resultItemNav"></div>
-              </Link>
-              <FavoriteBorderIcon
-                className="m-auto"
-                sx={{
-                  color: "red",
-                }}
-                id="favoriteButton1"
-                onClick={(event) => {
-                  // event.currentTarget.style.color = "black";
-                  handleFavoriteButton("remove", item);
-                }}
-              ></FavoriteBorderIcon>
-            </div>
-          );
-        })}
+                  }}
+                >
+                  {
+                    <img
+                      className="h-96 object-cover w-screen md:h-40"
+                      src={
+                        images[index].photos[0]
+                          ? images[index].photos[0].src.large
+                          : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F07%2F13%2FUltimate-Veggie-Burgers-FT-Recipe-0821.jpg"
+                      }
+                      alt={images[index].photos.alt}
+                    />
+                  }
+                  <h5
+                    className="text-center font-serif font-bold"
+                    id="itemHeading"
+                  >
+                    {item.title}
+                  </h5>
+                  <p className="text-xs text-center font-light">{`${item.ingredients.substring(
+                    0,
+                    100
+                  )}...`}</p>
+                  <div className="resultItemNav"></div>
+                </Link>
+                <FavoriteBorderIcon
+                  className="m-auto"
+                  sx={{
+                    color: "red",
+                  }}
+                  id="favoriteButton1"
+                  onClick={(event) => {
+                    handleFavoriteButton("remove", item);
+                  }}
+                ></FavoriteBorderIcon>
+              </div>
+            );
+          })
+        ) : (
+          <Box
+            sx={{
+              backgroundColor: "white",
+              width: "100vw",
+              height: '100vh',
+              display: "grid",
+              margin: 'auto',
+              justifyContent: "center",
+            }}
+          >
+            <CircularProgress
+              sx={{ display: "grid", justifyContent: "center" }}
+            />
+          </Box>
+        )}
       </div>
     </div>
   );
