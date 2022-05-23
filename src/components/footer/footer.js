1fd8ca4760a9cce5f8ea69c0d29bac2881acb429
kartikey <kartikeykumar150>
2022-05-23 09:30:03 +0530

    Added the solid heart icon

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import  { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
