570642364764ef56bd6452b14a5add9ad7f4daa2
kartikey <kartikeykumar150>
2022-05-23 11:03:42 +0530

    Removed the FontAwesomeIcon in footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faHeart } from "@fortawesome/free-solid-svg-icons"; 
 
 const Footer = () => {
