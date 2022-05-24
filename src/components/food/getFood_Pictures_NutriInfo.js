9fbc566ab8fa73e149bc47ab9d09373f86591d68
kartikey <kartikeykumar150>
2022-05-24 18:37:18 +0530

    Added the custom hook for fetching food recipe, pictures, and nutritional info

---
+++ src/components/food/getFood_Pictures_NutriInfo.js
@@ -0,0 +1,129 @@
+const useGetFoodItem_Pictures_items = (name, id) => {
+
+    const [item, setItem] = useState();
+    const [pictures, setPictures] = useState();
+    const [ingredientList, setIngredientList] = useState([]);
+    const [nutriObj, setNutriObj] = useState({
+      sugar_g: 0,
+      fiber_g: 0,
+      serving_size_g: 0,
+      sodium_mg: 0,
+      potassium_mg: 0,
+      fat_saturated_g: 0,
+      fat_total_g: 0,
+      calories: 0,
+      cholesterol_mg: 0,
+      protein_g: 0,
+      carbohydrates_total_g: 0,
+    });
+
+    useEffect(() => {
+
+      getRecipesFromApiNinjas(name).then((itemArr) => {
+        const item = itemArr[id];
+        setItem(item);
+
+        const ingredients = item.ingredients.split("|");
+        setIngredientList(ingredients);
+
+        const promises = Array.from({ length: ingredients.length }).map(
+          (_, index) => {
+            return getNutri(ingredients[index]);
+          }
+        );
+        Promise.all(promises).then((list) => {
+          let tempNutriObj = Object.create(itemsObj);
+
+          for (let item of list) {
+            console.log(item);
+            for (let items of item.items) {
+              tempNutriObj = {
+                sugar_g: items.sugar_g + tempNutriObj.sugar_g,
+                fiber_g: items.fiber_g + tempNutriObj.fiber_g,
+                serving_size_g:
+                  items.serving_size_g + tempNutriObj.serving_size_g,
+                sodium_mg: items.sodium_mg + tempNutriObj.sodium_mg,
+                potassium_mg:
+                  items.potassium_mg + tempNutriObj.potassium_mg,
+                fat_saturated_g:
+                  items.fat_saturated_g + tempNutriObj.fat_saturated_g,
+                fat_total_g: items.fat_total_g + tempNutriObj.fat_total_g,
+                calories: items.calories + tempNutriObj.calories,
+                cholesterol_mg:
+                  items.cholesterol_mg + tempNutriObj.cholesterol_mg,
+                protein_g: items.protein_g + tempNutriObj.protein_g,
+                carbohydrates_total_g:
+                  items.carbohydrates_total_g +
+                  tempNutriObj.carbohydrates_total_g,
+              };
+            }
+          };
+          console.log(tempNutriObj);
+          setNutriObj(tempNutriObj);
+        });
+        getPhoto(itemArr[id], 6).then((images) => {
+          setPictures(images);
+        });
+
+        return itemArr[id];
+      });
+    }, []);
+    return [item, pictures, ingredientList, nutriObj];
+  };
+
+  console.log(useGetFoodItem_Pictures_items(name, id));
+  useEffect(() => {
+    const getFoodItemAndGetPictures = async () => {
+      setGotFood(false);
+      const item = await getFood(name, id);
+      setGotFood(true);
+      setItem(item);
+      setPicturesLoaded(false);
+      const tempIngredientList = item.ingredients.split("|");
+      setIngredientList(tempIngredientList);
+      getPhoto(item.title, 6).then((response) => {
+        setPictures(response.photos);
+        setPicturesLoaded(true);
+      });
+      const promises = Array.from({ length: 10 }).map(async (_, index) => {
+        const nutritionobj = getNutri(tempIngredientList[index]);
+        return nutritionobj;
+      });
+      Promise.all(promises).then((list) => {
+        setNutrientInfoLoaded(false);
+        let tempNutriObj = Object.create(itemsObj);
+        list.map((item) => {
+          const values = Object.values(item);
+          const items = values[0][0];
+          try {
+            tempNutriObj = {
+              sugar_g: items.sugar_g + tempNutriObj.sugar_g,
+              fiber_g: items.fiber_g + tempNutriObj.fiber_g,
+              serving_size_g:
+                items.serving_size_g + tempNutriObj.serving_size_g,
+              sodium_mg: items.sodium_mg + tempNutriObj.sodium_mg,
+              potassium_mg: items.potassium_mg + tempNutriObj.potassium_mg,
+              fat_saturated_g:
+                items.fat_saturated_g + tempNutriObj.fat_saturated_g,
+              fat_total_g: items.fat_total_g + tempNutriObj.fat_total_g,
+              calories: items.calories + tempNutriObj.calories,
+              cholesterol_mg:
+                items.cholesterol_mg + tempNutriObj.cholesterol_mg,
+              protein_g: items.protein_g + tempNutriObj.protein_g,
+              carbohydrates_total_g:
+                items.carbohydrates_total_g +
+                tempNutriObj.carbohydrates_total_g,
+            };
+            setNutrientInfoLoaded(true);
+          } catch (err) {
+            return;
+          }
+        });
+        setitemsObj(tempNutriObj);
+      });
+    };
+    getFoodItemAndGetPictures();
+    
+  }, []);
+
+  export default getFoodItemAndGetPictures;
