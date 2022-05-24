08fd5234243bf59c534de063378321d2448705d9
kartikey <kartikeykumar150>
2022-05-25 00:09:53 +0530

    Loaded the nutripe picture and set it as the logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,19 +2,20 @@ import React, { useContext } from "react";
 import { useRef } from "react";
 import {Link} from 'react-router-dom'
 import { FoodContext } from "../context/foodContext";
+import image from '../../Nutripe Logo.jpeg';
 
 const Header = () => {
   const ref = useRef(null);
   const { handleSearchSubmit, value, setValue } = useContext(FoodContext);
   return (
-    <div className="flex justify-between items-center w-screen h-fit rounded-bl-md rounded-br-md p-3 bg-[#fceaea7c]">
+    <div className="flex justify-between items-center rounded-bl-md rounded-br-md bg-[#c8bbaa]">
       <Link to="/">
-        <h2 className="text-[rgb(2,2,2)] text-xl">Nutripe</h2>
+        <img src={image} className='h-20 w-30' />
       </Link>
       <form onSubmit={handleSearchSubmit}>
         <label>
           <input
-            className="p-2 text-[rgb(2,2,2)] w-40 mr-0 md:mr-0 focus:outline-none rounded-md"
+            className="p-2 text-[rgb(2,2,2)] w-40 mr-0 md:mr-14 focus:outline-none rounded-md"
             required
             value={value}
             onChange={(e) => setValue(e.target.value)}
