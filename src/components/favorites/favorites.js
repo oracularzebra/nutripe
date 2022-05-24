9b73d206fd5ae4a266b2f6e2b7797a8ee09c5369
kartikey <kartikeykumar150>
2022-05-24 23:43:46 +0530

    Aligned the loading icon

--- src/components/favorites/favorites.js
+++ src/components/favorites/favorites.js
@@ -33,77 +33,82 @@ const Favorites = () => {
   }, [favorites]);
 
   return (
-    <div>
-      <div className="grid grid-cols-1 gap-4 m-2 md:grid-cols-5 md:m-1 md:mt-0 justify-center bg-slate-200">
-        {imagesLoaded ? (
-          favorites.map((item, index) => {
-            return (
-              <div
-                className="grid content-between justify-center items-center rounded-md border-2 border-blue-200"
-                key={index}
-              >
-                <Link
-                  to={`${item.title}`}
-                  onClick={() => {
-                    if (imagesLoaded) {
-                      const newItem = {
-                        ...item,
-                        image: images[index].photos[0].src.large,
-                      };
-                      setItem(newItem);
-                    } else {
-                      setItem(item);
-                    }
-                  }}
-                >
-                  {
-                    <img
-                      className="h-96 object-cover w-screen md:h-40"
-                      src={
-                        images[index].photos[0]
-                          ? images[index].photos[0].src.large
-                          : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F07%2F13%2FUltimate-Veggie-Burgers-FT-Recipe-0821.jpg"
-                      }
-                      alt={images[index].photos.alt}
-                    />
+    <div className="grid grid-cols-1 gap-4 h-screen overflow-scroll m-2 md:grid-cols-5 md:m-1 md:mt-0 justify-center bg-slate-200">
+      {imagesLoaded ? (
+        favorites.map((item, index) => {
+          return (
+            <div
+              className="grid items-stretch rounded-md border-2 border-blue-200"
+              key={index}
+            >
+              <Link
+                to={`${item.title}`}
+                onClick={() => {
+                  if (imagesLoaded) {
+                    const newItem = {
+                      ...item,
+                      image: images[index].photos[0].src.large,
+                    };
+                    setItem(newItem);
+                  } else {
+                    setItem(item);
                   }
-                  <h5
-                    className="text-center font-serif font-bold"
-                    id="itemHeading"
-                  >
-                    {item.title}
-                  </h5>
-                  <p className="text-xs text-center font-light">{`${item.ingredients.substring(
-                    0,
-                    100
-                  )}...`}</p>
-                  <div className="resultItemNav"></div>
-                </Link>
-                <FavoriteBorderIcon
-                  className="m-auto"
-                  sx={{
-                    color: "red",
-                  }}
-                  id="favoriteButton1"
-                  onClick={(event) => {
-                    handleFavoriteButton("remove", item);
-                  }}
-                ></FavoriteBorderIcon>
-              </div>
-            );
-          })
-        ) : (
-          <Box
-            style={{
-              backgroundColor: "white",
-              display: "grid",
-              justifyContent: "center",
+                }}
+              >
+                {
+                  <img
+                    className="w-screen object-cover rounded-md md:h-40 lg:h-96"
+                    src={
+                      images[index].photos[0]
+                        ? images[index].photos[0].src.large
+                        : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F07%2F13%2FUltimate-Veggie-Burgers-FT-Recipe-0821.jpg"
+                    }
+                    alt={images[index].photos.alt}
+                  />
+                }
+                <h5
+                  className="text-center font-serif font-bold"
+                  id="itemHeading"
+                >
+                  {item.title}
+                </h5>
+                <p className="text-xs text-center font-light">{`${item.ingredients.substring(
+                  0,
+                  100
+                )}...`}</p>
+                <div className="resultItemNav"></div>
+              </Link>
+              <FavoriteBorderIcon
+                className="m-auto"
+                sx={{
+                  color: "red",
+                }}
+                id="favoriteButton1"
+                onClick={() => {
+                  handleFavoriteButton("remove", item);
+                }}
+              ></FavoriteBorderIcon>
+            </div>
+          );
+        })
+      ) : (
+        <Box
+          sx={{
+            height: '100vh',
+            width: '100vw'
+          }}>
+          <CircularProgress
+            sx={{
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+              margin: "auto",
+              position: "absolute",
             }}
-          >
-            <CircularProgress />
-          </Box>
-        )}
-      </div>
+          />
+        </Box>
+      )}
     </div>
   );
 };
