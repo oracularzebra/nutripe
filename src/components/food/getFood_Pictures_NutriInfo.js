fdc070fa21390e0606777cca2c723fc1699ca970
kartikey <kartikeykumar150>
2022-05-25 09:54:22 +0530

    Emptied the dependency list in custom hook

--- src/components/food/getFood_Pictures_NutriInfo.js
+++ src/components/food/getFood_Pictures_NutriInfo.js
@@ -80,7 +80,7 @@ const useGetFoodItem_Pictures_items = (name, id) => {
 
         return itemArr[id];
       });
-    }, [name, id]);
+    }, []);
     return [item, pictures, ingredientList, nutriObj, gotFood, picturesLoaded, nutrientInfoLoaded];
   };
 
